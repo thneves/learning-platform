58b3975292e3834fc4d78b29afa47d8bc4bc844e
thneves <tsneves11@gmail.com>
2022-03-03 23:15:21 -0300

    Add slim language, and static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#landing_page"
 
-  get 'static_pages/landing_page'
-  get 'static_pages/privacy_policy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
